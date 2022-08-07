321756431256c527df536d3ed871ef1ac09036aa
uni2tw <uni2tw@gmail.com>
2022-08-08 07:15:46 +0800

    開始弄UI

--- CoreForm/Program.cs
+++ CoreForm/Program.cs
@@ -16,7 +16,7 @@ namespace CoreForm
             Application.SetCompatibleTextRenderingDefault(false);
 
 
-            Application.Run(new Form1());
+            Application.Run(new FormMain());
         }
     }
 }
