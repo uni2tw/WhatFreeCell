4823db2d97347e2592705f298a838acbb1968cb1
uni2tw <uni2tw@gmail.com>
2022-08-14 17:15:12 +0800

    minor

--- CoreForm/Controls/TableauContainer.cs
+++ CoreForm/Controls/TableauContainer.cs
@@ -6,7 +6,51 @@ using System.Threading.Tasks;
 
 namespace FreeCellSolitaire.Controls
 {
-    internal class TableauContainer
+    public class FoundactionsContainer
     {
+        private List<FoundactionColumnContainer> _columnContainers;
+        public FoundactionsContainer()
+        {
+            _columnContainers = new List<FoundactionColumnContainer>(4);
+        }
+        
+    }
+
+    public class GeneralColumnContainer
+    {
+
+    }
+
+
+    public class FoundactionColumnContainer : GeneralColumnContainer
+    {
+
+    }
+
+    public class HomecellColumnContainer : GeneralColumnContainer
+    {
+
+    }
+    public class TableauColumnContainer : GeneralColumnContainer
+    {
+
+    }
+
+    public class HomecellsContainer
+    {
+        private List<HomecellColumnContainer> _columnContainers;
+        public HomecellsContainer()
+        {
+            _columnContainers = new List<HomecellColumnContainer>(4);
+        }
+    }
+
+    public class TableauContainer
+    {
+        private List<TableauColumnContainer> _tableauColumns;
+        public TableauContainer()
+        {
+            _tableauColumns = new List<TableauColumnContainer>(4);
+        }
     }
 }
