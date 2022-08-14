b51a2ee2acea2eed51d874054ef471cb7fb15037
uni2tw <uni2tw@gmail.com>
2022-08-14 12:32:52 +0800

    minor

--- FreeCellSolitaire.Core/ZoneModels/Tableau.cs
+++ FreeCellSolitaire.Core/ZoneModels/Tableau.cs
@@ -61,7 +61,7 @@ namespace FreeCellSolitaire.Core.GameModels
                 return true;
             }
         }
-        public int ColumnCapacity => 20;
+        public int ColumnCapacity => 19;
 
         public int ColumnCount => 8;
 
