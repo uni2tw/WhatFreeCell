57ef966b26087397e819535f7b7ccb271422d6a6
uni2tw <uni2tw@gmail.com>
2022-08-26 07:51:03 +0800

    0826 CardControl

---
+++ CoreForm/UI/CardControl.cs
@@ -0,0 +1,50 @@
+﻿using CoreForm.UI;
+using CoreForm.Utilities;
+using FreeCellSolitaire.Core.CardModels;
+using FreeCellSolitaire.Core.GameModels;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FreeCellSolitaire.UI
+{
+    public class CardControl : PictureBox
+    {
+        public Card Card { get; set; }
+        
+        public Image ActiveImage { get; private set; }
+
+        public CardControl(int cardWidth, int cardHeight)
+        {
+            PictureBox viewControl = new PictureBox
+            {
+                Location = new Point(0, 0),
+                Width = cardWidth,
+                Height = cardHeight,
+                SizeMode = PictureBoxSizeMode.Normal,
+                Image = null
+            };
+            InitImage();
+        }
+
+        private void InitImage()
+        {
+            var assembly = System.Reflection.Assembly.GetEntryAssembly();
+            Stream resource = assembly
+                .GetManifestResourceStream("FreeCellSolitaire.assets.img." + GetImageFileName());
+            Image img = Image.FromStream(resource);
+            this.Image = img;
+            this.ActiveImage = img.DrawAsNegative();
+        }
+
+        private string GetImageFileName()
+        {
+            return Card.Suit.ToString() + "-" + Card.Number.ToString("00");
+        }
+    }
+}
